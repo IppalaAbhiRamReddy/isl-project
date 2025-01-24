6ecd363cd9a54053ce7549b006ee2174eb89e39e
Ippala Abhiram Reddy <abhiramreddyippala@gmail.com>
2025-01-24 19:51:32 +0530

    Homepage name correction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./HomePage";
+import HomePage from "./Homepage";
 import LoginPage from "./LoginPage";
 import SignupPage from "./SignupPage";
 import IslTranslation from "./Translation";
